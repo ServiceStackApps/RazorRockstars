ee426e0113aaa1f14abe4789af4aeac8c91adbc1
Demis Bellot <demis.bellot@gmail.com>
2012-09-03 18:39:16 -0400

    First commit of Razor Rockstars

---
+++ lib/copy.bat
@@ -0,0 +1,4 @@
+PUSHD "..\..\ServiceStack\release\"
+CALL copy.bat
+POPD
+COPY ..\..\ServiceStack\release\latest\ServiceStack\*  .\
---
+++ src/RazorRockstars.WebHost/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RazorRockstars.WebHost.Global" Language="C#" %>
