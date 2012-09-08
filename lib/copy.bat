10b93d75b66995c797f83379f5daf0cd928d2b8e
Demis Bellot <demis.bellot@gmail.com>
2012-09-08 06:34:37 -0400

    Add latest SS.dlls, change @Model.ToJson() to @Model.AsRawJson() to by-pass HTML encoding
    Change to port 2000 to avoid conflicts with test projects

--- lib/copy.bat
+++ lib/copy.bat
@@ -2,3 +2,4 @@ PUSHD "..\..\ServiceStack\release\"
 CALL copy.bat
 POPD
 COPY ..\..\ServiceStack\release\latest\ServiceStack\*  .\
+COPY ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\Sqlite32.Mono\* .\
