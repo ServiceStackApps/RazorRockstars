64c00bbfc780b455d07ff834e04d0ea813f1082b
Demis Bellot <demis.bellot@gmail.com>
2012-09-16 06:05:00 -0400

    Change SelfHost.exe to exit with Ctrl+C instead of any key

--- src/RazorRockstars.SelfHost/Program.cs
+++ src/RazorRockstars.SelfHost/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using ServiceStack.Logging;
 using ServiceStack.Logging.Support.Logging;
 using ServiceStack.Text;
@@ -15,8 +16,9 @@ namespace RazorRockstars.SelfHost
             appHost.Init();
             appHost.Start("http://*:2001/");
 
-            "\n\nType any key to quit..".Print();
-            Console.ReadKey();
+            "\n\nListening on http://*:2001/..".Print();
+            "Type Ctrl+C to quit..".Print();
+            Thread.Sleep(Timeout.Infinite);
         }
     }
 }
