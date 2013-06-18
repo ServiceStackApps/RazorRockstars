81cfea97c24523740cfa0c269c23107b2d8e55b2
Demis Bellot <demis.bellot@gmail.com>
2013-06-18 02:54:50 -0400

    Update copy to reflect latest Razor2 re-write.

--- lib/copy.bat
+++
@@ -1,5 +0,0 @@
-PUSHD "..\..\ServiceStack\release\"
-CALL copy.bat
-POPD
-COPY ..\..\ServiceStack\release\latest\ServiceStack\*  .\
-COPY ..\..\ServiceStack\release\latest\ServiceStack.OrmLite\Sqlite32.Mono\* .\
